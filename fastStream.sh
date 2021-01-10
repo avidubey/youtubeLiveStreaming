f0d5bfb633f247aed92b741885f1470b678af9ca
Avi <avinash007ster@gmail.com>
2021-01-10 02:17:47 -0800

    Update and rename stream.sh to fastStream.sh
    Using default fps of input as output fps

---
+++ fastStream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# ENVIRONMENT VARIABLES
+# Expecting RTSP_URL and RTMP_URL environment variables setup in the system. Please read README file for instructions and steps
+# Using default input fps as output fps
+
+echo "Starting slow live streaming to just keep live streaming alive!!"
+ffmpeg -fflags +igndts -i $RTSP_URL -c:a copy  -filter:v -f flv  $RTMP_URL
