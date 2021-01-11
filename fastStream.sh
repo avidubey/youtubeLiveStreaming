a1dcff98f815109b084b11f3ca5560705f004916
avi <avinash007ster@gmail.com>
2021-01-11 08:36:54 -0800

    fixing bin/bash statement to allow cron files access the environment variables defined in bashrc

--- fastStream.sh
+++ fastStream.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-
+#!/bin/bash -l
 
 # ENVIRONMENT VARIABLES
 # Expecting RTSP_URL and RTMP_URL environment variables setup in the system. Please read README file for instructions and steps
 # Using default input fps as output fps
-
+echo "Killing existing ffmpeg first for clean up and fresh start"
+killall ffmpeg
 echo "Starting slow live streaming to just keep live streaming alive!!"
 ffmpeg -fflags +igndts -i $RTSP_URL -c:v copy -c:a copy -f flv $RTMP_URL
--- slowStream.sh
+++ slowStream.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 # ENVIRONMENT VARIABLES
 # Expecting RTSP_URL and RTMP_URL environment variables setup in the system. Please read README file for instructions and steps
@@ -6,5 +6,7 @@
 # With youtube if the live streaming is stopped, it would end the stream and it needs manual intervention to restart the stream using website
 # The slow streaming keeps the youtube stream alive as well as saves bandwidth as we are streaming just 1 frame per second.
 
+echo "Killing existing ffmpeg first for clean up and fresh start"
+killall ffmpeg
 echo "Starting slow live streaming to just keep live streaming alive!!"
 ffmpeg -fflags +igndts -i $RTSP_URL -c:a copy -filter:v fps=1 -g 4 -f flv $RTMP_URL
