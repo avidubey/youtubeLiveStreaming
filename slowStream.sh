62a6ca4df7d6f2ec59cdf6551524a7c1f4493a48
Avi <avinash007ster@gmail.com>
2021-01-10 18:24:16 -0800

    fixing slow stream with fps 1

--- slowStream.sh
+++ slowStream.sh
@@ -7,6 +7,6 @@
 # With youtube if the live streaming is stopped, it would end the stream and it needs manual intervention to restart the stream using website
 # The slow streaming keeps the youtube stream alive as well as saves bandwidth as we are streaming just 1 frame per second.
 
-FPS = 2
+FPS = 1
 echo "Starting slow live streaming to just keep live streaming alive!!"
-ffmpeg -fflags +igndts -i $RTSP_URL -c:v copy -filter:v fps=fps=$FPS -f flv  $RTMP_URL
+ffmpeg -fflags +igndts -i $RTSP_URL -c:a copy -filter:v fps=fps=1 -g 60 -f flv  $RTMP_URL 
