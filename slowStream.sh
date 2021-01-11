8f75aa1a38aa499dca0d789c6da71efda5b4d44b
Avi <avinash007ster@gmail.com>
2021-01-11 11:45:14 -0800

    Updating instructions for slowStream.sh

--- slowStream.sh
+++ slowStream.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -l
 
-# ENVIRONMENT VARIABLES
-# Expecting RTSP_URL and RTMP_URL environment variables setup in the system. Please read README file for instructions and steps
+##
 # The slow streaming is for saving bandwidth during night time when there is little to no action of birds. No point streaming at higher fps
 # With youtube if the live streaming is stopped, it would end the stream and it needs manual intervention to restart the stream using website
 # The slow streaming keeps the youtube stream alive as well as saves bandwidth as we are streaming just 1 frame per second.
+##
+
+# ENVIRONMENT VARIABLES
+# Expecting RTSP_URL and RTMP_URL environment variables setup in the system. Please read README file for instructions and steps
 
 echo "Killing existing ffmpeg first for clean up and fresh start"
 killall ffmpeg
