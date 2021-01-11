882ad3ead6fc862bfe2a730c35f74e4523a96250
Avi <avinash007ster@gmail.com>
2021-01-11 11:44:31 -0800

    Updating comments for fastStream.sh

--- fastStream.sh
+++ fastStream.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -l
 
+##
+# Use this script to stream at regular high quality bandwidth. The cron job in my setup runs this script during day time and slowStream.sh script during night
+# when there is no activity. This helps saves bandwidth cost.
+##
+
 # ENVIRONMENT VARIABLES
 # Expecting RTSP_URL and RTMP_URL environment variables setup in the system. Please read README file for instructions and steps
 # Using default input fps as output fps
