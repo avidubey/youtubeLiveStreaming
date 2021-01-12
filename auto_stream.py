4802b6b6ea59e77441b80127c98fa8b7413d9e34
avi <avinash007ster@gmail.com>
2021-01-11 22:43:22 -0800

    Fixing the environment variable part of code

--- auto_stream.py
+++ auto_stream.py
@@ -2,6 +2,7 @@
 # This script automatically figures out if its day time or night time based on the location and starts fast/slow stream accordingly
 
 import os
+import sys
 import requests
 import json
 from datetime import datetime, time, timedelta
@@ -10,6 +11,9 @@ from datetime import datetime, time, timedelta
 # These will be unique to your use cases but would look something like this
 # RTSP_URL = "rtsp://username:password@cameraIP/live" for your wyze cam
 # RTMP_URL = "rtmp://a.rtmp.youtube.com/live2/<your stream key here>"
+
+RTSP_URL = os.environ['RTSP_URL']
+RTMP_URL = os.environ['RTMP_URL']
 
 # Get your location's environment variables from https://sunrise-sunset.org/api
 lattitude = 47.6694141
@@ -33,9 +37,17 @@ sunset_time = time(int(sunset[11:13]), int(sunset[14:16])) # Change sunset into 
 print("Killing existing processes first to avoid duplicate processes")
 os.system('killall ffmpeg')
 
-if time_now > sunrise_time and time_now < sunset_time:  # In between sunrise and sunset, so run faster stream	
+# Check if there is override argument to run specifically for day time or night time
+# example : override for day time : python3 autoStream.py true
+# example : override for night time : python3 autoStream.py false
+isDaytime = False
+if len(sys.argv) > 1 :
+	isDaytime = sys.argv[1]
+	print(f'override daytime flag set as {isDaytime}')
+
+if isDaytime is True or time_now > sunrise_time and time_now < sunset_time:  # In between sunrise and sunse
 	print("Starting fast streaming as its day time")
-	os.system('ffmpeg -fflags +igndts -i $RTSP_URL -c:a copy -filter:v -f flv $RTMP_URL')
+	os.system(f'ffmpeg -fflags +igndts -i {RTSP_URL} -c:v copy -c:a copy -f flv {RTMP_URL}')
 else:
 	print("Starting slow streaming as its night time")
-	os.system('ffmpeg -fflags +igndts -i $RTSP_URL -c:a copy -filter:v fps=1 -g 4 -f flv $RTMP_URL')
+	os.system(f'ffmpeg -fflags +igndts -i {RTSP_URL} -c:a copy -filter:v fps=1 -g 4 -f flv {RTMP_URL}')
